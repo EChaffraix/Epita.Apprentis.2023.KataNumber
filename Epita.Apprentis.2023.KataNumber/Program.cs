78ea5bb128d3b0a473c0dbaa8366cf5e31cb3159
Emmanuel CHAFFRAIX <echaffraix@talentsoft.com>
2021-11-16 09:50:08 +0100

    first commit

---
+++ Epita.Apprentis.2023.KataNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Epita.Apprentis._2023.KataNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
